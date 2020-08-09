e2bece0cb9d5081e4cb0f541b9179b562e803cad
Dominik <dominik.atrasik@primaris.eu>
2020-08-09 22:50:29 +0200

    Added angular themplate application

--- ArchitectureProject.Infrastructure/ArchitectureProjectDbContext.cs
+++ ArchitectureProject.Infrastructure/ArchitectureProjectDbContext.cs
@@ -1,14 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Data;
 using ArchitectureProject.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ArchitectureProject.Infrastructure
 {
-    public class ArchitectureProjectDbContext : DbContext
+    public sealed class ArchitectureProjectDbContext : DbContext
     {
+        public IDbConnection Connection { get; set; }
+
         public ArchitectureProjectDbContext(DbContextOptions<ArchitectureProjectDbContext> options)
             : base(options)
         {
+            this.Connection = this.Database.GetDbConnection();
         }
 
         public DbSet<Product> Products { get; set; }
